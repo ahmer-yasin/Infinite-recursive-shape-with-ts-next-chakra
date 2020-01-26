679445f4e2e2f8e699ef25b4ffcb148d80104b21
Ahmer Yasin <ahmer@rideairlift.com>
2020-01-27 02:55:55 +0500

    modify test

--- src/modules/shape/__tests__/Shape.test.tsx
+++ src/modules/shape/__tests__/Shape.test.tsx
@@ -1,21 +1,30 @@
 /* eslint-env jest */
 // import React from 'react'
-// import { shallow } from 'enzyme'
+import { shallow } from 'enzyme'
 
-// import Shape from './../Shape';
+import Shape from './../Shape';
+import ShapeState from './../Shape';
+const data = require('../testData')
 
 
 describe('test', () => {
   it('Test method 1', () => {
+    const app = shallow(<Shape />)
     expect(1).toEqual(1)
-  })
+  });
 
   it('draw data', () => {
-  })
+    let newShape = new Shape(ShapeState);
+    newShape.setState({ output: '' });
+    console.log(typeof data);
+    for (const element of data) {
+      newShape.setState({ width: element.input.split(",")[0] });
+      newShape.setState({ height: element.input.split(",")[1] });
+      newShape.setState({ padding: element.input.split(",")[2] });
 
-  it('changes the text of email', () => {
-   })
 
-  it('changes the text of login button after clicking it', () => {
-   })
+      newShape.state.output === element.input.pixelArrayJson? console.log(true): console.log(false);
+    }
+    expect(1).toEqual(1);
+  });
 })
